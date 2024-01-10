007c3d9181ec5731e1ae04f8c7857c38f2fe325d
salman1256 <salman1256@yahoo.com>
2024-01-10 10:25:26 +0530

    Final Commit

---
+++ Employee.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Employee
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Designation { get; set; }
+    public DateTime DOJ { get; set; }
+}
